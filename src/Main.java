fa9bc6b71fd804ab4443c7e374649f61fe7f935d
AlenaQA <alyona.trefilova@gmailcom>
2018-09-06 12:58:41 +0700

    Comment has been changed.

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,7 @@
 public class Main {
     public static  void main (String [] args) {
         System.out.println("Ciao!");
-        System.out.println("One more comment");
+        System.out.println("Comment is changed");
         System.out.println("Push it to GitHub");
     }
 }
