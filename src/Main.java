ad1fa23b623503985f7b5803077ad9763418bb40
AlenaQA <alyona.trefilova@gmailcom>
2018-09-06 12:55:21 +0700

    Pushing to GitHub.

--- src/Main.java
+++ src/Main.java
@@ -2,5 +2,6 @@ public class Main {
     public static  void main (String [] args) {
         System.out.println("Ciao!");
         System.out.println("One more comment");
+        System.out.println("Push it to GitHub");
     }
 }
