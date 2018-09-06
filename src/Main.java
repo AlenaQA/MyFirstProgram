93e52e6e1b9d038dfe720f9adea3aeeaec146ef6
AlenaQA <alyona.trefilova@gmailcom>
2018-09-06 13:00:13 +0700

    Comment has been changed. One more line has been added.

--- src/Main.java
+++ src/Main.java
@@ -3,5 +3,6 @@ public class Main {
         System.out.println("Ciao!");
         System.out.println("Comment is changed");
         System.out.println("Push it to GitHub");
+        System.out.println("Line is added");
     }
 }
