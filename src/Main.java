c6466fd3c6700f95b79b446e44c3e194a9861b3c
AlenaQA <alyona.trefilova@gmailcom>
2018-09-06 13:09:10 +0700

    Commit message for the branch 'alter'.

--- src/Main.java
+++ src/Main.java
@@ -3,5 +3,7 @@ public class Main {
         System.out.println("Ciao!");
         System.out.println("Comment is changed");
         System.out.println("Push it to GitHub");
+
+        System.out.println("Text in alter");
     }
 }
